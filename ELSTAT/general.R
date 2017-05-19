50fe97e8d2a714c94381a3128eb6a8833f5435b4
Saeid Amiri <saeid.amiri1@gmail.com>
2017-05-18 20:17:29 -0500

    Update general.R

--- ELSTAT/general.R
+++ ELSTAT/general.R
@@ -1,15 +1,17 @@
 FreqTable<-function(obs, nclass ,delta=.1){
-  range0<-seq(min(obs)-delta, max(obs)+delta,length=nclass)
+  nclass0<-(nclass+1)
+  range0<-seq(min(obs)-delta, max(obs)+delta,length=nclass0)
   l0<- -length(range0)
   low0<-range0[l0]
   high0<-range0[-1]
   
-  factor0 <- factor(cut(obs, breaks=range0))
+  factor0 <- (cut(obs, breaks=range0))
   xout <- data.frame(table(factor0),ClassMid=(high0+low0)/2)
   
-  xout2 <<- data.frame(Class=xout$factor0,ClassMid=xout$ClassMid,FreQ=xout$Freq,cumFreq = cumsum(xout$Freq), Relative = prop.table(xout$Freq),cumRel=cumsum(prop.table(xout$Freq)))
+  xout2 <- data.frame(Class=xout$factor0,ClassMid=xout$ClassMid,FreQ=xout$Freq,cumFreq = cumsum(xout$Freq), Relative = prop.table(xout$Freq),cumRel=cumsum(prop.table(xout$Freq)))
   return(xout2)
 }
+
 
 
 plotnorm<-function(mean, sd, lb, ub){
